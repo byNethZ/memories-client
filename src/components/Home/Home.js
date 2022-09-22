a209c4c761be967d9fdafcec2a1f3630a29032d1
NethZ <bynethz@gmail.com>
2022-09-21 22:37:37 -0500

    Adding styles for Home, PostDetails, and modified redux for query posts by search

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,38 +1,106 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import {
+  Container,
+  Grow,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from "@material-ui/core";
 import Posts from "../Posts/Posts";
+import { useNavigate, useLocation } from "react-router-dom";
+import ChipInput from "material-ui-chip-input";
 import Form from "../Form/Form";
 import { useDispatch } from "react-redux";
-import { getPosts } from "../../actions/posts";
+import { getPosts, getPostsBySearch } from "../../actions/posts";
 import useStyles from "./../../styles";
-import Pagination from './../Pagination'
+import Pagination from "./../Pagination";
+
+import useStylesHome from "./styles";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 function Home() {
-    const [currentId, setCurrentId] = useState(null)
-    const classes = useStyles();
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
-      dispatch(getPosts())
-    }, [currentId, dispatch])
+  const [currentId, setCurrentId] = useState(null);
+  const classes = useStylesHome();
+  const dispatch = useDispatch();
+  const query = useQuery();
+  const navigate = useNavigate();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
+
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  const handleKeyPress = (evt) => {
+    if (evt.keyCode === 13) {
+      //search post
+      searchPost();
+    }
+  };
+
+  const handleAdd = (tag) => setTags([...tags, tag]);
+
+  const handleDelete = (tagToDelete) =>
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+
+  const searchPost = () => {
+    if(search.trim() || tags){
+      //dispatch => fetch search post
+      dispatch(getPostsBySearch({search, tags: tags.join(',') }))
+    } else {
+      navigate('/');
+    }
+  }
 
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
-          className={classes.mainContainer}
+          className={classes.gridContainer}
           container
           justifyContent="space-between"
           alignItems="stretch"
           spacing={3}
         >
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
 
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              postition="static"
+              color="inherit"
+            >
+              <TextField
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={handleKeyPress}
+              />
+              <ChipInput
+                style={{ margin: "10px 0" }}
+                value={tags}
+                onAdd={handleAdd}
+                onDelete={handleDelete}
+                label="Search tags"
+                variant="outlined"
+              />
+              <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary" >Search</Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
-            <Paper className={classes.pagination} elevation={6} >
+            <Paper className={classes.pagination} elevation={6}>
               <Pagination />
             </Paper>
           </Grid>
